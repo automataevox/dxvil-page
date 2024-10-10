d1c435ddcf67980be50c257fe8d6227bebb9f980
Jaroslav Maša <musicbyblakk@gmail.com>
2024-10-10 09:16:53 +0200

    Added footer

--- layouts/default.tsx
+++ layouts/default.tsx
@@ -1,8 +1,13 @@
-import { Link } from "@nextui-org/link";
+import { FaSpotify } from "react-icons/fa";
+import { FaTwitch } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { SiApplemusic } from "react-icons/si";
 
 import { Head } from "./head";
 
 import { Navbar } from "@/components/navbar";
+import Link from "next/link";
 
 export default function DefaultLayout({
   children,
@@ -12,22 +17,28 @@ export default function DefaultLayout({
   return (
     <div className="relative flex flex-col h-screen">
       <Head />
-      <div className="noise w-screen h-screen absolute opacity-20 grayscale"/>
+      <div className="noise w-screen h-screen absolute opacity-20 grayscale" />
       {/* <Navbar /> */}
       <main className="mix-blend-screen">
         {children}
       </main>
-      {/* <footer className="w-full flex items-center justify-center py-3">
-        <Link
-          isExternal
-          className="flex items-center gap-1 text-current"
-          href="https://nextui-docs-v2.vercel.app?utm_source=next-pages-template"
-          title="nextui.org homepage"
-        >
-          <span className="text-default-600">Powered by</span>
-          <p className="text-primary">NextUI</p>
+      <footer className="w-full flex items-center justify-center py-3 absolute bottom-0 gap-3">
+        <Link href="https://open.spotify.com/artist/1Ngynwc6bFIKGzRcOrBAnx">
+          <FaSpotify size={20} />
         </Link>
-      </footer> */}
+        <Link href="https://twitch.tv/dxvilmusic">
+          <FaTwitch size={20} />
+        </Link>
+        <Link href="https:/youtube.com/@dxvilmusic">
+          <FaYoutube size={20} />
+        </Link>
+        <Link href="https://instagram.com/dxvilmusic">
+          <FaInstagram size={20} />
+        </Link>
+        <Link href="https://music.apple.com/us/artist/dxvil/1581545014">
+          <SiApplemusic size={20} />
+        </Link>
+      </footer>
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,9 +5,9 @@ export default function IndexPage() {
   return (
     <DefaultLayout>
       <section className="grid place-content-center w-screen h-screen">
-        
+          
           <Image src={logo} className="w-[15rem] animate-pulse" alt="logo"/>
-        
+
       </section>
     </DefaultLayout>
   );
