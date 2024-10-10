a61a0cef0601b0a4f09e8c93cbb7214a01001f29
Jaroslav Maša <musicbyblakk@gmail.com>
2024-10-10 02:25:48 +0200

    First Commit

---
+++ layouts/default.tsx
@@ -0,0 +1,33 @@
+import { Link } from "@nextui-org/link";
+
+import { Head } from "./head";
+
+import { Navbar } from "@/components/navbar";
+
+export default function DefaultLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="relative flex flex-col h-screen">
+      <Head />
+      <div className="noise w-screen h-screen absolute opacity-20 grayscale"/>
+      {/* <Navbar /> */}
+      <main className="mix-blend-screen">
+        {children}
+      </main>
+      {/* <footer className="w-full flex items-center justify-center py-3">
+        <Link
+          isExternal
+          className="flex items-center gap-1 text-current"
+          href="https://nextui-docs-v2.vercel.app?utm_source=next-pages-template"
+          title="nextui.org homepage"
+        >
+          <span className="text-default-600">Powered by</span>
+          <p className="text-primary">NextUI</p>
+        </Link>
+      </footer> */}
+    </div>
+  );
+}
---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import DefaultLayout from "@/layouts/default";
+import logo from "@/public/logo.png";
+import Image from "next/image";
+export default function IndexPage() {
+  return (
+    <DefaultLayout>
+      <section className="grid place-content-center w-screen h-screen">
+        
+          <Image src={logo} className="w-[15rem] animate-pulse" alt="logo"/>
+        
+      </section>
+    </DefaultLayout>
+  );
+}
